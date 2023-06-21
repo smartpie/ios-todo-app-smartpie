6f3adce04d5a8194f3f16959858731ef9853fdf4
NeSPie <newsmartpie@yandex.ru>
2023-06-21 12:11:03 +0300

    Colors added, main storyboard removed

--- ToDoList/Controller/ViewController.swift
+++ ToDoList/Controller/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
+        view.backgroundColor = .systemTeal
     }
 
 
